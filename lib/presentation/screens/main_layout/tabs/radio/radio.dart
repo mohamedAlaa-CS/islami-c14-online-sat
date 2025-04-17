267c2c5858f23e5bc760b6f3ff765e58cd925225
Mohamed Alaa <mohamedalaacs@gmail.com>
2025-04-17 22:48:14 +0200

    change radio color to green

--- lib/presentation/screens/main_layout/tabs/radio/radio.dart
+++ lib/presentation/screens/main_layout/tabs/radio/radio.dart
@@ -10,3 +10,22 @@ class Radio extends StatelessWidget {
     );
   }
 }
+
+/// git steps
+///  task Radio Screen
+///  1- clone the repo
+///  2- update to development
+///  3- create a new branch
+///  4- write code
+///  5-  commit to this code with good message dec what you have done
+///  6-  update branch ===> skip now  (to solve conflict if found)
+///  7- push to github
+///  8- pull request   from this branch to (master - development)
+///  9- merge to (master - development)  ===> owner only or team lead
+///
+///
+///  i am have next task
+///  1- update to development
+///  2- create a new branch
+///  ........... same steps
+///
